ebbb019458c06f00d7fd923217274a8841c77d8c
yoshisuke1013 <shin10132022@gmail.com>
2025-08-30 20:46:49 +0900

    ページネーション機能

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,20 +3,28 @@ import { Video } from "../../modules/videos/video.entity";
 import { videoRepository } from "../../modules/videos/video.repository";
 import VideoCard from "./VideoCard";
 import "./Home.css";
+import { useSearchParams } from "react-router-dom";
 
 function Home() {
   const [videos, setVideos] = useState<Video[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [queryParams, setQueryParams] = useSearchParams();
+  const page = parseInt(queryParams.get("page") || "1");
+  const [totalPages, setTotalPages] = useState<number | undefined>(undefined);
 
   useEffect(() => {
     fetchVideos();
-  }, []);
+  }, [page]);
 
   const fetchVideos = async () => {
     try {
       setIsLoading(true);
-      const { videos } = await videoRepository.find();
+      const { videos, pagination } = await videoRepository.find({
+        page,
+        limit: 1,
+      });
       setVideos(videos);
+      setTotalPages(pagination.totalPages);
     } catch (error) {
       console.error(error);
     } finally {
@@ -34,13 +42,29 @@ function Home() {
         ))}
       </div>
       <div className="pagination">
-        <button className="pagination-btn prev-btn">
+        <button
+          className="pagination-btn prev-btn"
+          disabled={page === 1}
+          onClick={() => {
+            setQueryParams({
+              page: String(page - 1),
+            });
+          }}
+        >
           <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
             <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
           </svg>
           前へ
         </button>
-        <button className="pagination-btn next-btn">
+        <button
+          className="pagination-btn next-btn"
+          disabled={page === totalPages}
+          onClick={() => {
+            setQueryParams({
+              page: String(page + 1),
+            });
+          }}
+        >
           次へ
           <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
             <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
